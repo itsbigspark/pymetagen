de28c8945399a6481b54042ccabb4d716c9d3ab3
vdiaz <vadrienzo@gmail.com>
2024-01-21 20:36:51 +0000

    Adds a excution test for the inspect command

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,7 +19,7 @@ class TestCli:
             "input_xlsx_path",
         ],
     )
-    def test_cli(
+    def test_cli_metadata(
         self,
         input_path: str,
         tmp_dir_path: Path,
@@ -30,7 +30,7 @@ class TestCli:
         runner = CliRunner()
         outpath: Path = tmp_dir_path / "meta.csv"
         result = runner.invoke(
-            cli, ["-i", input_path, "-o", outpath, "-m", mode]
+            cli, ["metadata", "-i", input_path, "-o", outpath, "-m", mode]
         )
 
         assert result.exit_code == 0
@@ -39,16 +39,37 @@ class TestCli:
         assert outpath.is_file()
         assert outpath.stat().st_size > 0
 
-    def test_input_not_exist(
+    def test_metadata_input_not_exist(
         self, tmp_dir_path: Path, mode: MetaGenSupportedLoadingModes
     ) -> None:
         runner = CliRunner()
         outpath: Path = tmp_dir_path / "meta.csv"
         result = runner.invoke(
-            cli, ["-i", "not_exist_path", "-o", outpath, "-m", mode]
+            cli,
+            ["metadata", "-i", "not_exist_path", "-o", outpath, "-m", mode],
         )
 
         assert result.exit_code == 2
         assert "does not exist" in result.output
 
         assert not outpath.exists()
+
+    @pytest.mark.parametrize(
+        "input_path",
+        [
+            "input_csv_path",
+            "input_parquet_path",
+            "input_xlsx_path",
+        ],
+    )
+    def test_cli_inspect(
+        self,
+        input_path: str,
+        request: pytest.FixtureRequest,
+        mode: MetaGenSupportedLoadingModes,
+    ) -> None:
+        input_path = request.getfixturevalue(input_path)
+        runner = CliRunner()
+        result = runner.invoke(cli, ["inspect", "-i", input_path, "-m", mode])
+
+        assert result.exit_code == 0
